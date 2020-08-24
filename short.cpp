46b5362519be6bafb81a54fea902c06839671302
Noor <nooremf@gmai.com>
2020-08-24 10:55:16 +0600

    added some things

---
+++ short.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    int a=15, b=3, i=5;
+
+    if (a>b && ++i<b)
+    {
+    }
+
+    cout << i << endl;
+    
+    return 0;
+}
