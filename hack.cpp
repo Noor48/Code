be4b3b57483eda7b8c06463f56ea623b858d6bf9
Noor <nooremf@gmai.com>
2020-09-14 12:45:27 +0600

    i've weakened

---
+++ hack.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int sum (int a, int b)
+{
+    return a+b;
+}
+
+double sum (double a, double b)
+{
+    return a+b;
+}
+
+double sum (double a, double b, double c)
+{
+    return a+b+c;
+}
+
+int main()
+{
+    int a, b;
+    double c, d,e=0;
+    cout << "Enter number; ";
+    cin >> a >> b >> c >> d;
+    cout << sum(a,b) << endl;
+    cout << sum(c,d) << endl;
+    e = sum(a,b);
+    cout << sum(a,b,e) << endl;
+    
+    return 0;
+}
