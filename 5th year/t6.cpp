b95b721a25b76f0148823367403af7a705fd38f8
Noor E Mustafa Ferdous <nooremf@gmail.com>
2022-09-11 11:16:52 +0600

    working

--- 5th year/t6.cpp
+++ 5th year/t6.cpp
@@ -26,7 +26,7 @@ double phi(complex <double> A[5][5])
     complex <double> A2[5][5] = {0};
     complex <double> A3[5][5] = {0};
     complex <double> A4[5][5] = {0};
-    /*
+    
     for(int i=0; i<5-1; i+=1)
     {
         for(int j=i+1; j<5; j+=1)
@@ -45,7 +45,7 @@ double phi(complex <double> A[5][5])
         p=r1;
         A[i][i] = p;
     }
-    */
+    
     for(int i=0; i<5; i+=1)
     {
         for(int j=0; j<5; j+=1)
@@ -113,32 +113,16 @@ double phi(complex <double> A[5][5])
     return S;
 }
 
-double force()
+double dh(complex <double> (&dh)[5][5], complex <double> phi[5][5])
 {
-    double r1,r2,p,q,sum=0, sum2=0,P;
-    complex <double> B[5][5] = {0};
+    
+}
+
+double P_phi(complex <double> B[5][5])
+{
+    double r1,r2,p,q,sum=0, sum2=0, P;
+
     complex <double> B2[5][5] = {0};
-    complex <double> B3[5][5] = {0};
-    complex <double> B4[5][5] = {0};
-
-    for(int i=0; i<5-1; i+=1)
-    {
-        for(int j=i+1; j<5; j+=1)
-        {
-            Box(r1,r2);
-            p=r1;
-            q=r2;
-            B[i][j] = complex(p/sqrt(2),q/sqrt(2));
-            B[j][i] = complex(p/sqrt(2),-q/sqrt(2)); 
-        }
-    }
-
-    for(int i=0; i<5; i+=1)
-    {
-        Box(r1,r2);
-        p=r1;
-        B[i][i] = p; 
-    }
 
     for(int i=0; i<5; i+=1)
     {
@@ -153,35 +137,64 @@ double force()
 
     for(int i=0; i<5; i+=1)
     {
-        for(int j=0; j<5; j+=1)
-        {
-            for(int k=0; k<5; k+=1)
-            {
-                B3[5][5] += B2[i][k]*B[k][j];
-            }
-        }
-    }
-    
-    for(int i=0; i<5; i+=1)
-    {
         sum += B[i][i].real();
     }
 
-    for(int i=0; i<5; i+=1)
-    {
-        sum2 += B3[i][i].real();
-    }
+    P = 0.5*sum; 
 
-    P = 0.5*sum;
     return P;
 }
 
 double H(complex <double> A[5][5], complex <double> B[5][5])
 {
-    phi(A);
-
-    return 0;
+    return phi(A) + P_phi(B);
 } 
+
+double molecular(complex <double> (&phi)[5][5],double& hi, double& hf)
+{
+    double r1,r2,p,q,nt=40,dt=1, sum=0;
+    complex <double> P[5][5] = {0};
+
+    for(int i=0; i<5-1; i+=1)
+    {
+        for(int j=i+1; j<5; j+=1)
+        {   
+            Box(r1,r2);
+            p=r1;
+            q=r2
+            P[i][j] = complex(p/sqrt(2),q/sqrt(2));
+            P[j][i] = complex(p/sqrt(2),-q/sqrt(2));
+        }
+    }
+
+    for(int i=0; i<5; i+=1)
+    {
+        Box(r1,r2);
+        p = r1;
+        P[i][i] = p;
+    }
+
+    hi = H(phi,P);
+
+    for(int i=0; i<5; i+=1)
+    {
+        for(int j=0; j<5; j+=1)
+        {
+            phi[i][j] += 0.5*P[i][j]*dt;
+        }
+    }
+
+    for(int i=1; i<=nt; i+=1)
+    {
+        for(int j=0; j<5; j+=1)
+        {
+            for(int k=0; k<5; k+=1)
+            {
+                P[i][j] -= force
+            }
+        }
+    }
+}
 int main()
 {
     complex <double> A[5][5] = {0};
