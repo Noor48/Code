361ebbe1393f86523fb19c58f206fefaa262837e
Noor E Mustafa Ferdous <nooremf@gmail.com>
2022-09-13 18:13:43 +0600

    working

--- 5th year/t7.cpp
+++ 5th year/t7.cpp
@@ -1,56 +1,12 @@
 #include<iostream>
 #include<cmath>
-#include<cstdlib>
 #include<fstream>
-#include<ctime>
+#include<cstdlib>
 #include<complex>
+#include<ctime>
 using namespace std;
 
-tuple <complex <double>> phi;
-double Box(double& x, double& y)
+double random()
 {
-    double p, q;
-
-    p = (double)rand()/(double)RAND_MAX;
-    q = (double)rand()/(double)RAND_MAX;
-
-    x = sqrt(-2*log(p))*sin(2*M_PI*q);
-    y = sqrt(-2*log(p))*cos(2*M_PI*q);
-
-    return 0;
-}
-
-double S(double x)
-{
-    return 0.5*pow(x,2);
-}
-double H(double x, double p)
-{
-    return 0.5*pow(p,2) + S(x);
-}
-
-double dh(double x)
-{
-    return x;
-}
-double molecular(double& x, double& hi, double& hf)
-{
-    double p, r1,r2,nt=40,dt=1;
-    Box(r1,r2);
-    p=r1;
-
-    hi = H(x,p);
-
-    x += 0.5*p*dt;
-
-    for(int i=0; i<nt; i+=1)
-    {
-        p -= dh(x)*dt;
-        x += p*dt;
-    }
-
-    p -= dh(x)*dt;
-    x += 0.5*p*dt;
-
-    return 0;
+    return (double)rand()/(double)RAND_MAX;
 }
