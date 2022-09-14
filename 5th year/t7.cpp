f6bde3221a4006098f40d5c209c4904e0ba2d2a3
Noor E Mustafa Ferdous <nooremf@gmail.com>
2022-09-14 19:13:44 +0600

    working

--- 5th year/t7.cpp
+++ 5th year/t7.cpp
@@ -10,3 +10,8 @@ double random()
 {
     return (double)rand()/(double)RAND_MAX;
 }
+
+double matrix(complex<double> A[n][n], complex<double> (&A2)[n][n], complex<double> (&A3)[n][n], complex<double> (&A4)[n][n], double s, double s2, double s3, double s4)
+{
+    
+}
