5a88da618dac0c1a85f54fd1ecb2e8d3261bbd61
Noor E Mustafa Ferdous <nooremf@gmail.com>
2023-05-20 22:04:50 +0600

    working

--- masters/test4.py
+++ masters/test4.py
@@ -6,15 +6,22 @@ x = 1
 p = 1
 a=[]
 b=[]
+c=[]
+d=[]
 s1=0
 s2=0
+s3=0
 #t = np.linspace(0, 10, 100)
 #x = x0 * np.exp(t)
 #p = p0 * np.exp(-t)
 for t in np.arange(0,10,0.001):
-    s1 += x*np.exp(t)
-    s2 += p*np.exp(-t)
-    a.append(s1)
-    b.append(s2)    
-plt.plot(a,b)
+    s1 = x*np.exp(t)
+    s2 = p*np.exp(-t)
+    s3 = s2*s1
+    b.append(s1)
+    c.append(s2)
+data = np.column_stack((b,c))
+np.savetxt("test4.dat", data)    
+plt.plot(b,c)
+plt.xlim(0,6)
 plt.show()
